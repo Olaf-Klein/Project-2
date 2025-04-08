379140820c7aa66e3bbb8d0acb900ab416ce81bb
Olaf Klein <159010471+Olaf-Klein@users.noreply.github.com>
2025-04-08 12:29:51 +0200

    DB_connection

---
+++ Database/DB_Connection.php
@@ -0,0 +1,17 @@
+<?php
+  $servername = "sql210.infinityfree.com";
+  $username = "if0_38649015";
+  $password = "HdUB2Ufhpw";
+  $dbname = "if0_38649015_p2db";
+  $port = "3306";
+
+  $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+
+
+  $conn->close();
+?>
